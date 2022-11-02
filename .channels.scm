9de293078e09a3df8c34f5caea982eee2dfc71fb
Peter Polidoro <peter@polidoro.io>
2022-11-02 14:31:17 -0400

    Initial commit

---
+++ .channels.scm
@@ -0,0 +1,13 @@
+;; This file is generated automatically from .metadata.org
+;; File edits may be overwritten!
+(list (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "a0751e3250dfea7e52468c8090e18c3118d93a60")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
