02666d329316144e4d1320c0b928e6da90724b4c
Peter Polidoro <peter@polidoro.io>
2022-11-07 10:59:44 -0500

    Modify wiring

--- .channels.scm
+++ .channels.scm
@@ -5,7 +5,7 @@
         (url "https://git.savannah.gnu.org/git/guix.git")
         (branch "master")
         (commit
-          "a0751e3250dfea7e52468c8090e18c3118d93a60")
+          "8e54584d4448d37ddf8ae995bb545a181ba2493c")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
