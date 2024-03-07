a863a6fb099e5421ef20a81c2f626c8b58869f13
Peter Polidoro <peter@polidoro.io>
2024-03-07 13:35:24 -0500

    Upgrade to kicad 7.0.11

--- .metadata/guix/channels.scm
+++ .metadata/guix/channels.scm
@@ -1,19 +1,19 @@
 ;; This file is generated automatically from metadata
 ;; File edits may be overwritten!
 (list (channel
-        (name 'guix)
-        (url "https://git.savannah.gnu.org/git/guix.git")
-        (branch "master")
-        (commit
-          "21e4d6cd6913eca131f2c0fd0cd509fc843c7eb8")
-        (introduction
-          (make-channel-introduction
-            "9edb3f66fd807b096b48283debdcddccfea34bad"
-            (openpgp-fingerprint
-              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA"))))
-      (channel
         (name 'guix-janelia)
         (url "https://github.com/guix-janelia/guix-janelia.git")
         (branch "main")
         (commit
-          "08fe0b2ecb68d9e17dfa4802c79935ea6339d6d1")))
+          "08fe0b2ecb68d9e17dfa4802c79935ea6339d6d1"))
+      (channel
+        (name 'guix)
+        (url "https://git.savannah.gnu.org/git/guix.git")
+        (branch "master")
+        (commit
+          "7f2f1bcddae64c48f793cde15c21617fa706c157")
+        (introduction
+          (make-channel-introduction
+            "9edb3f66fd807b096b48283debdcddccfea34bad"
+            (openpgp-fingerprint
+              "BBB0 2DDF 2CEA F6A8 0D1D  E643 A2A0 6DF2 A33A 54FA")))))
